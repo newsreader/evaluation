c03a4d8f241d090e81980938f69cc74a7397c1c6
Anne-Lyse Minard <minard@mahfuz.fbk.eu>
2015-10-21 12:05:55 +0200

    rename MISC by O

--- nerc-evaluation/01-get-first-5-sentences-from-gold.sh
+++ nerc-evaluation/01-get-first-5-sentences-from-gold.sh
@@ -2,4 +2,4 @@
 
 folder=$1
 
-for i in $folder/*; do time cat $i | perl -pe 's/^\n/JAR!!/g' | perl -pe 's/\n/ /g' | perl -pe 's/JAR!!/\n/g' | head -n 6 | perl -pe 's/ /\n/g' > $i.five ; done
+for i in $folder/*; do cat $i | perl -pe 's/^\n/JAR!!/g' | perl -pe 's/\n/ /g' | perl -pe 's/JAR!!/\n/g' | head -n 6 | perl -pe 's/ /\n/g' | perl -pe 's/[BI]-MISC/O/g' > $i.five ; done
